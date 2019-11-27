125cb9fbeef1b09bc095c89661de181e0a5fe3f7
Karl McCarron <karl.mccarron@eggplant.io>
2019-11-27 12:53:37 +0000

    WIP on day 23

--- src/aoc_problems/day_23.rs
+++ src/aoc_problems/day_23.rs
@@ -4,10 +4,14 @@ use std::fmt;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+use std::iter;
 use std::result;
+use std::ops::{Add, Sub, AddAssign};
 use std::str::FromStr;
 
 use std::collections::{BinaryHeap, BTreeMap, BTreeSet, HashMap, HashSet};
+
+use itertools::Itertools;
 
 use regex::Regex;
 
@@ -41,7 +45,19 @@ impl Coordinate {
         Coordinate { x, y, z }
     }
 
-    fn distance_from(&self, other: &Self) -> usize {
+    fn surrounding_squares(self) -> Vec<Coordinate> {
+        let ds_list: Vec<_> = iter::repeat(-1..2).take(3).multi_cartesian_product().collect();
+        ds_list.into_iter()
+            // .filter(|ds| ds[0] != 0 || ds[1] != 0 || ds[2] != 0)
+            .map(|ds| {
+                let dx = ds[0];
+                let dy = ds[1];
+                let dz = ds[2];
+                Coordinate { x: self.x + dx, y: self.y + dy, z: self.z + dz }
+            }).collect()
+    }
+
+    fn distance_from(&self, other: Self) -> usize {
         (
             (self.x - other.x).abs()
             + (self.y - other.y).abs()
@@ -59,6 +75,40 @@ impl Ord for Coordinate {
 impl PartialOrd for Coordinate {
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
         Some((self.z, self.y, self.x).cmp(&(other.z, other.y, other.x)))
+    }
+}
+
+impl Add for Coordinate {
+    type Output = Coordinate;
+
+    fn add(self, other: Coordinate) -> Coordinate {
+        Coordinate {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl Sub for Coordinate {
+    type Output = Coordinate;
+
+    fn sub(self, other: Coordinate) -> Coordinate {
+        Coordinate {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl AddAssign for Coordinate {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
     }
 }
 
@@ -86,7 +136,10 @@ impl Nanobot {
     }
 
     fn distance_from(&self, other: &Self) -> usize {
-        self.pos.distance_from(&other.pos)
+        self.pos.distance_from(other.pos)
+    }
+    fn is_in_range_of(&self, pos: Coordinate) -> bool {
+        self.pos.distance_from(pos) <= self.radius
     }
 }
 
@@ -105,7 +158,6 @@ impl FromStr for Nanobot {
         }
 
         if s.lines().count() != 1 {
-            println!("Cannot parse: {}", s);
             return err!("Only accepts 1 line");
         }
 
@@ -128,6 +180,10 @@ impl fmt::Display for Nanobot {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Nanobot at {} with radius {}", self.pos, self.radius)
     }
+}
+
+fn nanobots_in_range_of(nanobots: &Vec<Nanobot>, c: Coordinate) -> usize {
+    nanobots.iter().filter(|nanobot| nanobot.pos.distance_from(c) <= nanobot.radius).count()
 }
 
 pub fn q1(fname: String) -> usize {
@@ -159,6 +215,106 @@ fn _q1(nanobot_list: String) -> Result<usize> {
     )
 }
 
+pub fn q2(fname: String) -> usize {
+    let mut f = File::open(fname).expect("File not found");
+    let mut f_contents = String::new();
+
+    f.read_to_string(&mut f_contents).expect("Couldn't find file");
+    f_contents = f_contents.trim().to_string();
+
+    _q2(f_contents).unwrap()
+}
+
+fn _q2(nanobot_list: String) -> Result<usize> {
+    let nanobots: Result<Vec<Nanobot>> = nanobot_list
+        .trim()
+        .lines()
+        .map(|l| l.trim().parse())
+        .collect();
+
+    let nanobots = nanobots?;
+
+    let best_nanobot = nanobots.iter().max_by_key(|nanobot| nanobot.radius).ok_or("No nanobots!")?;
+
+    let mut current_position: Coordinate = best_nanobot.pos;
+    let mut nanobots_in_range = nanobots_in_range_of(&nanobots, current_position);
+    loop {
+        let mut distance_to_move = Coordinate::new(0, 0, 0);
+        for nanobot in &nanobots {
+            if !nanobot.is_in_range_of(current_position) {
+                let relative_coordinate = nanobot.pos - current_position;
+                let max_coord_dist = vec![
+                    relative_coordinate.x,
+                    relative_coordinate.y,
+                    relative_coordinate.z,
+                ].into_iter().map(|n| n.abs()).max().ok_or("No elements!")?;
+
+                if relative_coordinate.x.abs() == max_coord_dist {
+                    distance_to_move += Coordinate::new(relative_coordinate.x.signum(), 0, 0);
+                } else if relative_coordinate.y.abs() == max_coord_dist {
+                    distance_to_move += Coordinate::new(0, relative_coordinate.y.signum(), 0);
+                } else if relative_coordinate.z.abs() == max_coord_dist {
+                    distance_to_move += Coordinate::new(0, 0, relative_coordinate.z.signum());
+                }
+            }
+        }
+
+        // let distance_to_move = Coordinate {
+        //     x: distance_to_move.x.signum(),
+        //     y: distance_to_move.y.signum(),
+        //     z: distance_to_move.z.signum(),
+        // };
+
+        if distance_to_move == Coordinate::new(0, 0, 0) {
+            break;
+        }
+
+        current_position += distance_to_move;
+
+        if nanobots_in_range_of(&nanobots, current_position) < nanobots_in_range {
+            break;
+        }
+
+        nanobots_in_range = nanobots_in_range_of(&nanobots, current_position);
+    }
+
+    println!("Got to first part");
+    println!("Current position = {}", current_position);
+
+    loop {
+        let max_nanobots_in_range: usize = current_position
+            .surrounding_squares()
+            .into_iter()
+            .map(|coord| nanobots_in_range_of(&nanobots, coord))
+            .max()
+            .ok_or("No coordinates surrounding position")?;
+
+        let mut coords_to_go_to: Vec<Coordinate> = current_position
+            .surrounding_squares()
+            .into_iter()
+            .filter(|&c| nanobots_in_range_of(&nanobots, c) == max_nanobots_in_range)
+            .collect();
+        coords_to_go_to.sort_by_key(|c| c.distance_from(Coordinate::new(0, 0, 0)));
+
+        let coord_to_go_to: Coordinate = *coords_to_go_to.get(0).ok_or("No coordinates!")?;
+        if coord_to_go_to == current_position {
+            break;
+        }
+
+        current_position = coord_to_go_to;
+
+        println!("Current position is now {} with nanobots {}", current_position, max_nanobots_in_range);
+    }
+
+    println!("Position chosen = {}", current_position);
+    println!("{}", nanobots_in_range_of(&nanobots, Coordinate::new(11, 12, 12)));
+    println!("{}", nanobots_in_range_of(&nanobots, Coordinate::new(12, 12, 12)));
+
+    Ok(
+        current_position.distance_from(Coordinate::new(0, 0, 0))
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -180,4 +336,17 @@ mod tests {
         );
     }
 
+    #[test]
+    fn day23_q2() {
+        assert_eq!(
+            _q2("
+                pos=<10,12,12>, r=2
+                pos=<12,14,12>, r=2
+                pos=<16,12,12>, r=4
+                pos=<14,14,14>, r=6
+                pos=<50,50,50>, r=200
+                pos=<10,10,10>, r=5
+            ".to_string()).unwrap(), 36
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod aoc_problems;
 
 fn main() {
     let now = Instant::now();
-    let result = aoc_problems::day_23::q1("./inputs/day23.txt".to_string());
+    let result = aoc_problems::day_23::q2("./inputs/day23.txt".to_string());
     let elapsed = now.elapsed();
     println!("Answer: {:?}", result);
     println!("Elapsed time: {:?}", elapsed);
